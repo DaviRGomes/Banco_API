a73af0e9bec62f919147311eef75accb5a5d3bde
Davi Gomes <davirosagomesvini@gmail.com>
2025-04-03 15:31:48 -0300

    Initial commit

---
+++ src/main/java/com/dev/bank/Santander_API/SantanderApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.bank.Santander_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/bank/Santander_API/SantanderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.bank.Santander_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
