434266aa0482b031d069275e2172a59dd47ef8b5
Davi Gomes <davirosagomesvini@gmail.com>
2025-04-07 11:24:49 -0300

    Add repository

---
+++ src/main/java/com/dev/bank/Santander_API/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.dev.bank.Santander_API.repository;
+
+
+import com.dev.bank.Santander_API.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
